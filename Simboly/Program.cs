7641c73bd8697101f7238b62222e2180ff89bb35
khrom <khrom@DESKTOP-DA2UCIR>
2024-11-26 22:03:30 +0200

    Добавьте файлы проекта.

---
+++ Simboly/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {    
+        char[] symbols = new char[11];
+
+        Console.WriteLine("Введите 11 символов:");
+        for (int i = 0; i < symbols.Length; i++)
+        {
+            symbols[i] = Console.ReadKey().KeyChar; 
+            Console.Write(" "); 
+        }
+        for (int i = 0; i < symbols.Length; i++)
+        {
+            if (char.IsLower(symbols[i]))
+            {
+                symbols[i] = char.ToUpper(symbols[i]);
+            }
+        }
+        Console.WriteLine("\nИзмененный массив символов:");
+        foreach (char c in symbols)
+        {
+            Console.Write(c + " ");
+        }
+    }
+}
